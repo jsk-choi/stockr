7b92229ad1a7f87e63b5fcb36b906f02c336f7ef
whats-happening-rightnow <>
2020-12-11 14:06:32 -0800

    .gitignore fix

--- stockr.py/_secrets.py
+++
@@ -1,3 +0,0 @@
-iex_key = "pk_6936d6bbead54838ab45b0f845ece345"
-#iex_key = "Tpk_bc69ad6503f345cd9c0cb48dabf16b17"
-db_conn = 'DRIVER={SQL Server};SERVER=192.168.1.10;DATABASE=stockr;UID=sa;PWD=yS2uqF6hN0gpOqPk'
