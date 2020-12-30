9dc6d6419eb56656c9ef4c507ba53721778a3ddb
Joseph Choi <jsk.choi@gmail.com>
2020-12-30 15:38:13 -0800

    add text log failover if no db

--- stockr.py/db/log.py
+++ stockr.py/db/log.py
@@ -1,15 +1,38 @@
 import _secrets as sec
 import pyodbc 
+import time
+import os
+
+def logtext(msg):
+
+    date_time = time.strftime("%Y-%m-%d %H:%M:%S")
+    date_str = time.strftime("%Y%m%d")
+    flename = f"log_{date_str}.txt"
+
+    if not os.path.exists(flename):
+        open(flename, "w+").close()
+
+    open(flename, "a+").write(f'{date_time} : {msg}\n')
+
 
 def logmsg(msg):
 
     # msg = msg if len(msg) < 200 else msg[0:195] + ' ...'
     msg = msg.replace("'", "''")[0:2500]
 
-    conn = pyodbc.connect(sec.db_conn)
-    with conn:
-        cursor = conn.cursor()
-        with cursor:
-            insert_stmt = f"insert into [dbo].[Log] (SystemTime, Msg, Catg) values (getdate(), '{msg}', 'pyfo')"
-            cursor.execute(insert_stmt)
-            cursor.commit()
+    try:
+        conn = pyodbc.connect(sec.db_conn, timeout = 2)
+        with conn:
+            cursor = conn.cursor()
+            with cursor:
+                insert_stmt = f"insert into [dbo].[Log] (SystemTime, Msg, Catg) values (getdate(), '{msg}', 'pyfo')"
+                cursor.execute(insert_stmt)
+                cursor.commit()
+
+    except Exception as ex:
+        logtext(f"error: {ex}\n\t{msg}")
+
+
+
+logmsg('durrf')
+logmsg('again')
